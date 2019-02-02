4e3690fc8f3795017c873d53613e072e86dd4a27
hyolin97 <35828851+hyolin97@users.noreply.github.com>
2019-02-02 15:01:49 +0900

    ggg

--- Yello-Sakedong-Android-master/app/src/main/java/com/yello_sakedong/nexters/yellosakedong/main/viewmodel/MainActivity.java
+++ Yello-Sakedong-Android-master/app/src/main/java/com/yello_sakedong/nexters/yellosakedong/main/viewmodel/MainActivity.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -47,8 +48,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
+        Log.d("","fff");
 
 
 
