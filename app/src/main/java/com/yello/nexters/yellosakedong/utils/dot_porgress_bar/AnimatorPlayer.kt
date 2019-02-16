ad69d680267f719ae0735b4d8a374789e4acc781
KimHunJin <ysg01129@naver.com>
2019-02-16 13:19:27 +0900

    옐로 사케통 다이얼로그 완성

--- app/src/main/java/com/yello/nexters/yellosakedong/utils/dot_porgress_bar/AnimatorPlayer.kt
+++ app/src/main/java/com/yello/nexters/yellosakedong/utils/dot_porgress_bar/AnimatorPlayer.kt
@@ -2,8 +2,29 @@ package com.yello.nexters.yellosakedong.utils.dot_porgress_bar
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
 
 
-class AnimatorPlayer(animator: Array<Animator>) : AnimatorListenerAdapter() {
+class AnimatorPlayer(val animator: MutableList<Animator>) : AnimatorListenerAdapter() {
 
+    private var interrupted : Boolean = false
+
+    override fun onAnimationEnd(animation: Animator?) {
+        if(!interrupted) animate()
+    }
+
+    fun play() {
+        animate()
+    }
+
+    fun stop() {
+        interrupted = true
+    }
+
+    private fun animate() {
+        val set = AnimatorSet()
+        set.playTogether(animator)
+        set.addListener(this)
+        set.start()
+    }
 }
