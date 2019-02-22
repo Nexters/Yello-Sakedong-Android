8bec814bc78a5829d36f60ad520b1ff10b8ea052
hyolin97 <35828851+hyolin97@users.noreply.github.com>
2019-02-22 23:38:52 +0900

    input화면 최종//

--- app/src/main/java/com/yello/nexters/yellosakedong/input/InputActivity.java
+++ app/src/main/java/com/yello/nexters/yellosakedong/input/InputActivity.java
@@ -28,125 +28,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 import gun0912.tedbottompicker.TedBottomPicker;
+import io.reactivex.disposables.Disposable;
 
 
 public class InputActivity extends Activity {
 
-    ImageView iv_image;
-    Uri selectedUri;
-    final ArrayList<Uri> selectedUriList = new ArrayList<>();
+    private ImageView iv_image;
+    private List<Uri> selectedUriList;
+    private Uri selectedUri;
+    private Disposable singleImageDisposable;
+    private Disposable multiImageDisposable;
     private ViewGroup mSelectedImagesContainer;
     private RequestManager requestManager;
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
 
 
-        ViewGroup mSelectedImagesContainer;
-        RequestManager requestManager;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
-        ImageButton img=(ImageButton)findViewById(R.id.imageButton);
+        ImageButton img = (ImageButton) findViewById(R.id.imageButton);
         img.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                PermissionListener permissionlistener = new PermissionListener() {
-                    @Override
-                    public void onPermissionGranted() {
-
-                        TedBottomPicker bottomSheetDialogFragment = new TedBottomPicker.Builder(InputActivity.this)
-                                .setOnMultiImageSelectedListener(new TedBottomPicker.OnMultiImageSelectedListener() {
-                                    @Override
-                                    public void onImagesSelected(ArrayList<Uri> uriList) {
-                                    }
-                                })
-                                //.setPeekHeight(getResources().getDisplayMetrics().heightPixels/2)
-                                .setPeekHeight(1600)
-                                .showTitle(false)
-                                .setCompleteButtonText("Done")
-                                .setEmptySelectionText("No Select")
-//                                .setSelectedUriList()
-                                .create();
-
-//                        bottomSheetDialogFragment.show(getSupportFragmentManager());
-
-
-                    }
-
-                    @Override
-                    public void onPermissionDenied(List<String> deniedPermissions) {
-
-                    }
-
-
-                    public void onPermissionDenied(ArrayList<String> deniedPermissions) {
-                        Toast.makeText(InputActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
-                    }
-
-
-                };
-
-                TedPermission.with(InputActivity.this)
-                        .setPermissionListener(permissionlistener)
-                        .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
-                        .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        .check();
-
-            }
-        });
-
-
-
-
-
-
-        Button btn1=(Button)findViewById(R.id.regi);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), OutputActivity.class);
+                Intent intent = new Intent(getApplicationContext(), galleryaddActivity.class);
                 startActivityForResult(intent, 1);
                 finish();
 
-                // Dontbe stupid and stay calm and write some code
-
-
             }
         });
 
 
-    }
-    private void showUriList(ArrayList<Uri> uriList) {
-        // Remove all views before
-        // adding the new ones.
-        mSelectedImagesContainer.removeAllViews();
-
-        iv_image.setVisibility(View.GONE);
-        mSelectedImagesContainer.setVisibility(View.VISIBLE);
-
-        int wdpx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics());
-        int htpx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics());
 
 
-        for (Uri uri : uriList) {
 
-//            View imageHolder = LayoutInflater.from(this).inflate(R.layout.image_item, null);
-//            ImageView thumbnail = (ImageView) imageHolder.findViewById(R.id.media_image);
-//
-//            requestManager
-//                    .load(uri.toString())
-//                    .apply(new RequestOptions().fitCenter())
-//                    .into(thumbnail);
-//
-//            mSelectedImagesContainer.addView(imageHolder);
-//
-//            thumbnail.setLayoutParams(new FrameLayout.LayoutParams(wdpx, htpx));
+                Button btn1 = (Button) findViewById(R.id.regi);
+                btn1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), OutputActivity.class);
+                        startActivityForResult(intent, 1);
+                        finish();
 
-            // trst
+                        // Dontbe stupid and stay calm and write some code
 
 
-        }
+                    }
+                });
 
-    }
 
-}
+            }}
+
+
+
+
---
+++ app/src/main/java/com/yello/nexters/yellosakedong/input/galleryaddActivity.java
@@ -0,0 +1,15 @@
+package com.yello.nexters.yellosakedong.input;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.yello.nexters.yellosakedong.R;
+
+public class galleryaddActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.layout_output_404_page) ;
+
+    }
+}
